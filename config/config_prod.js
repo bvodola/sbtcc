75379afa5fb2b68b742d03c3f87d9cf90434caac
Brunno Vodola <bvodola@gmail.com>
2017-10-31 12:05:48 -0200

    Remove ignored files

--- config/config_prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  db: {
-    host     : 'dbmy0101.whservidor.com',
-    port     : '3306',
-    user     : 'sbtcc',
-    password : 'taraverde1958',
-    database : 'sbtcc'
-  }
-}
